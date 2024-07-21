4e3f8241743eb46b32cbec2c43435b393e5a8025
Mohd-Amirul-Asraf-Bin-Mohamad-Nasir <amirulasraf.binmohamadnasir@etiqa.com.my>
2024-07-22 06:56:05 +0800

    change build

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import dts from 'vite-plugin-dts'
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
-import { extname, relative, resolve } from 'path'
-import { fileURLToPath } from 'node:url'
-import { glob } from 'glob'
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 import { libInjectCss } from 'vite-plugin-lib-inject-css';
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,29 +15,33 @@ export default defineConfig({
 
   build: {
     lib: {
-      entry: resolve(__dirname, 'lib/index.ts'),
+      entry: {
+        index: resolve(__dirname, 'lib/index.ts'),
+        button: resolve(__dirname, 'lib/components/Button/index.tsx'),
+        input: resolve(__dirname, 'lib/components/Input/index.tsx'),
+      },
       formats: ['es']
     },
     copyPublicDir: false,
     rollupOptions: {
 
       external: ['react', 'react/jsx-runtime'],
-      input: Object.fromEntries(
-        glob.sync('lib/**/*.{ts,tsx}', {
-          ignore: ["lib/**/*.d.ts", "lib/**/*.css.ts"],
-        }).map(file => [
-          // The name of the entry point
-          // lib/nested/foo.ts becomes nested/foo
-          relative(
-            'lib',
-            file.slice(0, file.length - extname(file).length)
-          ),
-          // The absolute path to the entry file
-          // lib/nested/foo.ts becomes /project/lib/nested/foo.ts
-          fileURLToPath(new URL(file, import.meta.url))
-        ])
-      )
-      ,
+      // input: Object.fromEntries(
+      //   glob.sync('lib/**/*.{ts,tsx}', {
+      //     ignore: ["lib/**/*.d.ts", "lib/**/*.css.ts"],
+      //   }).map(file => [
+      //     // The name of the entry point
+      //     // lib/nested/foo.ts becomes nested/foo
+      //     relative(
+      //       'lib',
+      //       file.slice(0, file.length - extname(file).length)
+      //     ),
+      //     // The absolute path to the entry file
+      //     // lib/nested/foo.ts becomes /project/lib/nested/foo.ts
+      //     fileURLToPath(new URL(file, import.meta.url))
+      //   ])
+      // )
+      // ,
       output: {
         chunkFileNames: 'chunks/[name].[hash].js',
         assetFileNames: 'assets/[name][extname]',
